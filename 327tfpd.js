b88c3f4e5f34677268c3e0b59b50b6fa8492f452
Anziska <meekagrobler@gmail.com>
2022-11-10 13:02:15 +0200

    Reserve String solution

--- 327tfpd.js
+++ 327tfpd.js
@@ -1,3 +1,8 @@
-function solution(str) {
-	return str.split("").reverse().join("");
+function reverseString(str){
+	var newString = "";
+	for(var 1 = str.length - 1; i >= 0; i--){
+		newString += str[i];
+	}
+	return newString;
+
 }
