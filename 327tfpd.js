a426e89c1c21e2df65fefad997b0c165d31f13cb
Anziska <meekagrobler@gmail.com>
2022-11-10 15:00:01 +0200

    Boolean to Yes and No

--- 327tfpd.js
+++ 327tfpd.js
@@ -1,8 +1,7 @@
-function reverseString(str){
+function solution(str) {
 	var newString = "";
-	for(var 1 = str.length - 1; i >= 0; i--){
+	for (var i = str.length - 1; i >= 0; i--) {
 		newString += str[i];
 	}
 	return newString;
-
 }
